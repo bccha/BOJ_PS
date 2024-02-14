d8b1890718dfee3b2c4aa8d130af83d1d2225637
ByungChang Cha <morer@naver.com>
2024-02-14 23:45:11 +0900

    브루트포스 N과M 까지

---
+++ c++/basic/500_브루트포스/5_14500_테트로미노.cpp
@@ -0,0 +1,270 @@
+#include <bits/stdc++.h>
+
+#define ENDL "\n"
+#define Endl "\n"
+#define fastio ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+using namespace std;
+
+typedef unsigned char uchar;
+typedef unsigned short ushort;
+typedef unsigned int uint; // 0~약40억, int는 -20억~20억
+typedef long long llong;
+typedef unsigned long long ullong;
+
+// 입력 / 출력
+#if 0
+void strToken(const string &in, vector<string> &ret) {
+    stringstream ss(in);
+    string t;
+
+    while (ss >> t)
+    {
+        ret.push_back(t);
+    }
+}
+#endif
+
+#if 0
+void setPrecision(int p) {
+    cout << fixed;
+    cout.precision(p);
+}
+#endif
+
+#if 0
+// 수학
+// 최대공약수
+inline int getGCD(int a, int b) {
+    if (a < b) {
+        swap(a, b);
+    }
+     while (true) {
+        int remain = a % b;
+        if (remain == 0) {
+            break;
+        }
+        a = b;
+        b = remain;
+    }
+    return b;
+}
+
+// 최소공배수
+inline int getLCM(int a, int b) {
+    int gcd = getGCD(a, b);
+    return a * b / gcd;
+}
+
+// 에라스토테네스의 체
+// 반드시 return 받은 pointer를 delete 해주어야 한다.
+//    auto *ps = getSieve<1000000>();
+//    auto &sieve = *ps;
+//    delete ps;
+template<int maxVal>
+inline bitset<maxVal + 1>* getSieve() {
+    auto *ret = new bitset<maxVal + 1>();
+    auto &r = *ret;
+
+    r[0] = true;
+    r[1] = true;
+
+    for (int i = 2; i * i <= maxVal; ++i) {
+        if (r[i]) {
+            continue;
+        }
+        int j = i * i;
+        while (j <= maxVal) {
+            r[j] = true;
+            j += i;
+        }
+    }
+
+    return ret;
+}
+
+template <typename T>
+inline T factorial(T n) {
+    T ret = T(1);
+    if (n <= ret) {
+        return ret;
+    }
+    
+    while (n) {
+        ret *= n;
+        --n;
+    }
+
+    return ret;
+}
+
+template <typename T>
+inline T permutation(T n, T k) {
+    if (k > n) {
+        return T(0);
+    }
+
+    T ret = T(1);
+    for (T i = T(0); i != k; ++i) {
+        ret *= n;
+        --n;
+    }
+    return ret;
+}
+
+template <typename T>
+inline T combination(T n, T k) {
+    return permutation(n, k) / factorial(k);
+}
+#endif
+
+int N, M;
+int B[510][510];
+
+int T[2 + 1 + 4 * 2 + 2 * 2 + 4][4][4] = { // 2 + 1 + 8 + 4 + 4 = 19
+    {
+        {1, 1, 1, 1},
+        {0, 0, 0, 0},
+        {0, 0, 0, 0},
+        {0, 0, 0, 0}
+    },
+    {
+        {1, 0, 0, 0},
+        {1, 0, 0, 0},
+        {1, 0, 0, 0},
+        {1, 0, 0, 0}
+    },
+    {
+        {1, 1, 0, 0},
+        {1, 1, 0, 0},
+        {0, 0, 0, 0},
+        {0, 0, 0, 0}
+    },
+    {
+        {1, 1, 1, 0},
+        {0, 0, 1, 0},
+        {0, 0, 0, 0},
+        {0, 0, 0, 0}
+    },
+    {
+        {1, 1, 0, 0},
+        {1, 0, 0, 0},
+        {1, 0, 0, 0},
+        {0, 0, 0, 0}
+    },
+    {
+        {1, 0, 0, 0},
+        {1, 1, 1, 0},
+        {0, 0, 0, 0},
+        {0, 0, 0, 0}
+    },
+    {
+        {0, 1, 0, 0},
+        {0, 1, 0, 0},
+        {1, 1, 0, 0},
+        {0, 0, 0, 0}
+    },
+    {
+        {1, 1, 1, 0},
+        {1, 0, 0, 0},
+        {0, 0, 0, 0},
+        {0, 0, 0, 0}
+    },
+    {
+        {1, 0, 0, 0},
+        {1, 0, 0, 0},
+        {1, 1, 0, 0},
+        {0, 0, 0, 0}
+    },
+    {
+        {0, 0, 1, 0},
+        {1, 1, 1, 0},
+        {0, 0, 0, 0},
+        {0, 0, 0, 0}
+    },
+    {
+        {1, 1, 0, 0},
+        {0, 1, 0, 0},
+        {0, 1, 0, 0},
+        {0, 0, 0, 0}
+    },
+    {
+        {1, 1, 0, 0},
+        {0, 1, 1, 0},
+        {0, 0, 0, 0},
+        {0, 0, 0, 0}
+    },
+    {
+        {0, 1, 0, 0},
+        {1, 1, 0, 0},
+        {1, 0, 0, 0},
+        {0, 0, 0, 0}
+    },
+    {
+        {0, 1, 1, 0},
+        {1, 1, 0, 0},
+        {0, 0, 0, 0},
+        {0, 0, 0, 0}
+    },
+    {
+        {1, 0, 0, 0},
+        {1, 1, 0, 0},
+        {0, 1, 0, 0},
+        {0, 0, 0, 0}
+    },
+    {
+        {0, 1, 0, 0},
+        {1, 1, 1, 0},
+        {0, 0, 0, 0},
+        {0, 0, 0, 0}
+    },
+    {
+        {1, 0, 0, 0},
+        {1, 1, 0, 0},
+        {1, 0, 0, 0},
+        {0, 0, 0, 0}
+    },
+    {
+        {1, 1, 1, 0},
+        {0, 1, 0, 0},
+        {0, 0, 0, 0},
+        {0, 0, 0, 0}
+    },
+    {
+        {0, 1, 0, 0},
+        {1, 1, 0, 0},
+        {0, 1, 0, 0},
+        {0, 0, 0, 0}
+    },
+};
+
+int main() {
+    fastio
+
+    cin >> N >> M;
+    for (int i = 0; i != N; ++i) {
+        for (int j = 0; j != M; ++j) {
+            cin >> B[i][j];
+        }
+    }
+
+    int ret = 0;
+    for (int i = 0; i != N; ++i) {
+        for (int j = 0; j != M; ++j) {
+            for (int k = 0; k != 19; ++k) {
+                int sum = 0;
+                for (int ii = 0; ii != 4; ++ii) {
+                    for (int jj = 0; jj != 4; ++jj) {
+                        sum += B[i + ii][j + jj] * T[k][ii][jj];
+                    }
+                }
+                ret = max(ret, sum);
+            }
+        }
+    }
+
+    cout << ret << Endl;
+
+
+    return 0;
+}
---
+++ c++/basic/510_브루트포스_N과M/7_15656_N과M_7.cpp
@@ -0,0 +1,156 @@
+#include <bits/stdc++.h>
+
+#define ENDL "\n"
+#define Endl "\n"
+#define fastio ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+using namespace std;
+
+typedef unsigned char uchar;
+typedef unsigned short ushort;
+typedef unsigned int uint; // 0~약40억, int는 -20억~20억
+typedef long long llong;
+typedef unsigned long long ullong;
+
+// 입력 / 출력
+#if 0
+void strToken(const string &in, vector<string> &ret) {
+    stringstream ss(in);
+    string t;
+
+    while (ss >> t)
+    {
+        ret.push_back(t);
+    }
+}
+#endif
+
+#if 0
+void setPrecision(int p) {
+    cout << fixed;
+    cout.precision(p);
+}
+#endif
+
+#if 0
+// 수학
+// 최대공약수
+inline int getGCD(int a, int b) {
+    if (a < b) {
+        swap(a, b);
+    }
+     while (true) {
+        int remain = a % b;
+        if (remain == 0) {
+            break;
+        }
+        a = b;
+        b = remain;
+    }
+    return b;
+}
+
+// 최소공배수
+inline int getLCM(int a, int b) {
+    int gcd = getGCD(a, b);
+    return a * b / gcd;
+}
+
+// 에라스토테네스의 체
+// 반드시 return 받은 pointer를 delete 해주어야 한다.
+//    auto *ps = getSieve<1000000>();
+//    auto &sieve = *ps;
+//    delete ps;
+template<int maxVal>
+inline bitset<maxVal + 1>* getSieve() {
+    auto *ret = new bitset<maxVal + 1>();
+    auto &r = *ret;
+
+    r[0] = true;
+    r[1] = true;
+
+    for (int i = 2; i * i <= maxVal; ++i) {
+        if (r[i]) {
+            continue;
+        }
+        int j = i * i;
+        while (j <= maxVal) {
+            r[j] = true;
+            j += i;
+        }
+    }
+
+    return ret;
+}
+
+template <typename T>
+inline T factorial(T n) {
+    T ret = T(1);
+    if (n <= ret) {
+        return ret;
+    }
+    
+    while (n) {
+        ret *= n;
+        --n;
+    }
+
+    return ret;
+}
+
+template <typename T>
+inline T permutation(T n, T k) {
+    if (k > n) {
+        return T(0);
+    }
+
+    T ret = T(1);
+    for (T i = T(0); i != k; ++i) {
+        ret *= n;
+        --n;
+    }
+    return ret;
+}
+
+template <typename T>
+inline T combination(T n, T k) {
+    return permutation(n, k) / factorial(k);
+}
+#endif
+
+int N, M;
+bool v[9];
+int n[9];
+vector<int> l;
+
+void permute(int sel, int prev) {
+    if (sel == M) {
+        for (auto c : l) {
+            cout << c << " ";
+        }
+        cout << Endl;
+        return;
+    }
+
+    for (int i = 1; i <= N; ++i) {
+        /*
+        if (v[i]) {
+            continue;
+        }
+        */
+        //v[i] = true;
+        l.push_back(n[i]);
+        permute(sel + 1, i);
+        l.pop_back();
+        //v[i] = false;
+    }
+}
+
+int main() {
+    cin >> N >> M;
+    for (int i = 1; i <= N; ++i) {
+        cin >> n[i];
+    }
+    sort(n + 1, n + N + 1);
+    permute(0, 0);
+}
