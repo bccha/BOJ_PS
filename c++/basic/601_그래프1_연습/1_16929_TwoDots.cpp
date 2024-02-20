3630ec59618fff6d1912828a74b4a035a941e920
ByungChang Cha <morer@naver.com>
2024-02-21 01:02:45 +0900

    Two Dots 까지

---
+++ c++/basic/601_그래프1_연습/1_16929_TwoDots.cpp
@@ -0,0 +1,254 @@
+#include <bits/stdc++.h>
+
+#define ENDL    "\n"
+#define Endl    ENDL
+#define el      ENDL
+#define fastio ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+using namespace std;
+
+typedef unsigned char uchar;
+typedef unsigned short ushort;
+typedef unsigned int uint; // 0~약40억, int는 -20억~20억
+typedef long long llong;
+typedef unsigned long long ullong;
+
+// 입력 / 출력
+#if 0
+void strToken(const string &in, vector<string> &ret) {
+    stringstream ss(in);
+    string t;
+
+    while (ss >> t)
+    {
+        ret.push_back(t);
+    }
+}
+#endif
+
+#if 0
+void setPrecision(int p) {
+    cout << fixed;
+    cout.precision(p);
+}
+#endif
+
+#if 0
+// 수학
+// 최대공약수
+inline int getGCD(int a, int b) {
+    if (a < b) {
+        swap(a, b);
+    }
+     while (true) {
+        int remain = a % b;
+        if (remain == 0) {
+            break;
+        }
+        a = b;
+        b = remain;
+    }
+    return b;
+}
+
+// 최소공배수
+inline int getLCM(int a, int b) {
+    int gcd = getGCD(a, b);
+    return a * b / gcd;
+}
+
+// 에라스토테네스의 체
+// 반드시 return 받은 pointer를 delete 해주어야 한다.
+//    auto *ps = getSieve<1000000>();
+//    auto &sieve = *ps;
+//    delete ps;
+template<int maxVal>
+inline bitset<maxVal + 1>* getSieve() {
+    auto *ret = new bitset<maxVal + 1>();
+    auto &r = *ret;
+
+    r[0] = true;
+    r[1] = true;
+
+    for (int i = 2; i * i <= maxVal; ++i) {
+        if (r[i]) {
+            continue;
+        }
+        int j = i * i;
+        while (j <= maxVal) {
+            r[j] = true;
+            j += i;
+        }
+    }
+
+    return ret;
+}
+
+template <typename T>
+inline T factorial(T n) {
+    T ret = T(1);
+    if (n <= ret) {
+        return ret;
+    }
+    
+    while (n) {
+        ret *= n;
+        --n;
+    }
+
+    return ret;
+}
+
+template <typename T>
+inline T permutation(T n, T k) {
+    if (k > n) {
+        return T(0);
+    }
+
+    T ret = T(1);
+    for (T i = T(0); i != k; ++i) {
+        ret *= n;
+        --n;
+    }
+    return ret;
+}
+
+template <typename T>
+inline T combination(T n, T k) {
+    return permutation(n, k) / factorial(k);
+}
+
+bool next_permutation(vector<int> &l) {
+    int R = l.size();
+    int i = R - 2;
+    while (i >= 0 && l[i] >= l[i + 1]) {
+        --i;
+    }
+    if (i < 0) {
+        return false;
+    }
+    int target = -1;
+    for (int j = R - 1; j > i; --j) {
+        if (l[j] > l[i]) {
+            if (target == -1 || l[target] > l[j]) {
+                target = j;
+            }
+        }
+    }
+    iter_swap(l.begin() + i, l.begin() + target);
+    sort(l.begin() + i + 1, l.end());
+    return true;
+}
+#endif
+
+#if 0
+// string 반복  예) 'a' * 5
+// string s = string(5, 'a');
+
+// int to string
+// to_string(5);
+
+// string to int
+// stoi(s);
+#endif
+
+#if 0
+// basic code of DFS, BFS
+int N, M, V;
+vector<int> A[1001];
+int visited[1001];
+
+void dfs(int f) {
+    cout << f << " ";
+    for (auto n : A[f]) {
+        if (visited[n]) {
+            continue;
+        }
+        visited[n] = 1;
+        dfs(n);
+    }
+}
+
+void bfs(int f) {
+    deque<int> q;
+    visited[f] = 1;
+    q.push_back(f);
+
+
+    while (!q.empty()) {
+        int cur = q.front();
+        cout << cur << " ";
+        q.pop_front();
+        for (auto n : A[cur]) {
+            if (visited[n]) {
+                continue;
+            }
+            visited[n] = 1;
+            q.push_back(n);
+        }
+    }
+    cout << el;
+}
+#endif
+
+int N, M;
+char B[51][51];
+int visited[51][51];
+
+int dyx[4][2] = {
+    {-1, 0}, {1, 0}, {0, -1}, {0, 1}
+};
+
+bool dfs(int y, int x, int cnt) {
+    char cur = B[y][x];
+
+    for (int i = 0; i != 4; ++i) {
+        int ny = y + dyx[i][0];
+        int nx = x + dyx[i][1];
+
+        if (ny < 0 || ny >= N || nx < 0 || nx >= M) {
+            continue;
+        }
+
+        if (B[ny][nx] != B[y][x]) {
+            continue;
+        }
+
+        int v = visited[ny][nx];
+        if (v) {
+            if (cnt - v >= 2) {
+                return true;
+            }
+            continue;
+        }
+        visited[ny][nx] = cnt + 1;
+        if (dfs(ny, nx, cnt + 1)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+int main() {
+    fastio
+
+    cin >> N >> M;
+    for (int i = 0; i != N; ++i) {
+        cin >> B[i];
+    }
+
+    for (int i = 0; i != N; ++i) {
+        for (int j = 0; j != M; ++j) {
+            if (visited[i][j]) {
+                continue;
+            }
+            if (dfs(i, j, 1)) {
+                cout << "Yes" << endl;
+                return 0;
+            }
+        }
+    }
+    cout << "No" << endl;
+
+    return 0;
+}
